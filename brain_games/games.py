91ab82da4cb185e529895ee79de34df474e89e6a
grigarazh <grigarazh@yandex.ru>
2023-03-30 19:21:24 +0300

    fix: fixed brain-even description

--- brain_games/games.py
+++ brain_games/games.py
@@ -14,7 +14,7 @@ def print_game_description(game):
     """returns description for chosen game type"""
 
     if game == "even":
-        print('Answer "yes" if the number is even, otherwise answer "no"')
+        print('Answer "yes" if the number is even, otherwise answer "no".')
     elif game == "calc":
         print("What is the result of the expression?")
     elif game == "gcd":
