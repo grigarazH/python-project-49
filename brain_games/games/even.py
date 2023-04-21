f1c891e5c1e397b3a271bf029412e2db83fad845
Grigory Sukhotin <grigarazh72@gmail.com>
2023-04-21 22:41:17 +0300

    refactor: added is_even func

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -6,10 +6,14 @@ MAX_NUMBER = 100
 DESCRIPTION = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
+def is_even(number):
+    return number % 2 == 0
+
+
 def get_game():
     """returns even game data"""
 
     rand_num = random.randint(MIN_NUMBER, MAX_NUMBER)
-    correct_answer = "yes" if rand_num % 2 == 0 else "no"
+    correct_answer = "yes" if is_even(rand_num) else "no"
     question_string = str(rand_num)
     return question_string, correct_answer
